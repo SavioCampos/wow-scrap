ace3ee19e77ecbde55d75fdf7f6d929f08f7e2b6
Savio Campos <savio.c@dalt.cl>
2024-03-13 11:33:57 -0300

    Cambio de condicion por visibilidad

--- exchange.py
+++ exchange.py
@@ -48,7 +48,7 @@ driver.get(URL)
 try:
     # Define una espera explícita con un tiempo máximo de espera de 10 segundos o hasta que encuentre un elemento especifico
     elemento = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//td[@data-sort-value="Akama"]'))
+        EC.visibility_of_element_located((By.XPATH, '//td[@data-sort-value="Aegwynn"]'))
     )
 
     
